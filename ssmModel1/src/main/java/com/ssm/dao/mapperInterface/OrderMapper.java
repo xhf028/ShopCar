d9b303b28c2ff91fc7d0a52e3ab3b721df10f59a
xhf028 <952141821@qq.com>
2020-04-16 01:53:33 +0800

    2020.04.16 01:53

---
+++ ssmModel1/src/main/java/com/ssm/dao/mapperInterface/OrderMapper.java
@@ -0,0 +1,14 @@
+package com.ssm.dao.mapperInterface;
+
+import com.ssm.common.bean.Order;
+
+/**
+* @author 向鸿飞 
+* @version 创建时间：2020年4月15日 下午11:54:18
+* 类说明
+*/
+public interface OrderMapper {
+
+	//插入订单
+	void insertOrder(Order order);
+}
